d838ffb4c5b803d65ec8469181818800dc660b60
Prasad <pkp.patil93@gmail.com>
2022-03-02 23:06:20 +0530

    added couese put, post delete.

--- backend/controllers/courseController.js
+++ backend/controllers/courseController.js
@@ -20,50 +20,50 @@ router.get('/', (req, res) => {
     .catch(err => { console.log("Error in Retriving Courses") })
 });
 
-// router.get('/:id', (req, res) => {
-//     if (!ObjectId.isValid(req.params.id))
-//         return res.status(400).send(`No record with given id : ${req.params.id}`);
+router.get('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
 
-//     Teachers.findById(req.params.id, (err, doc) => {
-//         if (!err) { res.send(doc); }
-//         else { console.log('Error in Retriving Teacher :' + JSON.stringify(err, undefined, 2)); }
-//     });
-// });
+    Courses.findById(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Retriving Teacher :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
 
-// router.post('/', (req, res) => {
-//     var emp = new Teachers({
-//         name: req.body.name
-//     });
-//     emp.save((err, doc) => {
-//         if (!err) { res.send(doc); }
-//         else { console.log('Error in Teacher Save :' + JSON.stringify(err, undefined, 2)); }
-//     });
-// });
+router.post('/', (req, res) => {
+    var course = new Teachers({
+        name: req.body.name
+    });
+    course.save((err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in course Save :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
 
-// router.put('/:id', (req, res) => {
-//     if (!ObjectId.isValid(req.params.id))
-//         return res.status(400).send(`No record with given id : ${req.params.id}`);
+router.put('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
 
-//     var teacher = {
-//         name: req.body.name,
-//         position: req.body.position,
-//         office: req.body.office,
-//         salary: req.body.salary,
-//     };
-//     Teachers.findByIdAndUpdate(req.params.id, { $set: teacher }, { new: true }, (err, doc) => {
-//         if (!err) { res.send(doc); }
-//         else { console.log('Error in Teacher Update :' + JSON.stringify(err, undefined, 2)); }
-//     });
-// });
+    var course = {
+        course_id: 10,
+        course_name: req.body.course_name,
+        course_year: req.body.course_year,
+        course_sem: req.body.course_sem,
+    };
+    Courses.findByIdAndUpdate(req.params.id, { $set: course }, { new: true }, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Course Update :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
 
-// router.delete('/:id', (req, res) => {
-//     if (!ObjectId.isValid(req.params.id))
-//         return res.status(400).send(`No record with given id : ${req.params.id}`);
+router.delete('/:id', (req, res) => {
+    if (!ObjectId.isValid(req.params.id))
+        return res.status(400).send(`No record with given id : ${req.params.id}`);
 
-//     Teachers.findByIdAndRemove(req.params.id, (err, doc) => {
-//         if (!err) { res.send(doc); }
-//         else { console.log('Error in Teacher Delete :' + JSON.stringify(err, undefined, 2)); }
-//     });
-// });
+    Courses.findByIdAndRemove(req.params.id, (err, doc) => {
+        if (!err) { res.send(doc); }
+        else { console.log('Error in Teacher Delete :' + JSON.stringify(err, undefined, 2)); }
+    });
+});
 
 module.exports = router;
